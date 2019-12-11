34665c519bb5c4cc0df4ef8a80bb60d04095f9a4
Andre Azevedo <andre.azevedo@gmail.com>
2019-12-10 21:45:31 -0800

    Add control service

---
+++ src/control/control_service.cc
@@ -0,0 +1,61 @@
+#include "control/control_service.h"
+
+#include <atomic>
+#include <cassert>
+
+#include "control/motor.h"
+#include "control/vehicle.h"
+#include "runtime/mpsc_queue.h"
+#include "runtime/service_thread.h"
+
+namespace robocar {
+namespace control {
+
+ControlService::ControlService()
+    : queue_(kTaskQueueSize), thread_([this]() { run(); }, kExecutionRateHz) {}
+
+void ControlService::adjustVehicleState(double throttleDelta,
+                                        double steeringAngleDelta) {
+  bool scheduled = queue_.push([this, throttleDelta, steeringAngleDelta]() {
+    double newThrottle = vehicle_.adjustThrottle(throttleDelta);
+    double newSteeringAngle = vehicle_.adjustSteeringAngle(steeringAngleDelta);
+    throttle_.store(newThrottle, std::memory_order_relaxed);
+    steeringAngle_.store(newSteeringAngle, std::memory_order_relaxed);
+  });
+  assert(scheduled);
+}
+
+void ControlService::setThrottle(double throttle) {
+  bool scheduled = queue_.push([this, throttle]() {
+    vehicle_.setThrottle(throttle);
+    throttle_.store(throttle, std::memory_order_relaxed);
+  });
+  assert(scheduled);
+}
+
+void ControlService::setSteeringAngle(double steeringAngle) {
+  bool scheduled = queue_.push([this, steeringAngle]() {
+    vehicle_.setSteeringAngle(steeringAngle);
+    steeringAngle_.store(steeringAngle, std::memory_order_relaxed);
+  });
+  assert(scheduled);
+}
+
+void ControlService::setMotorState(Motor::State state) {
+  bool scheduled = queue_.push([this, state]() {
+    vehicle_.setState(state);
+    state_.store(state, std::memory_order_relaxed);
+  });
+  assert(scheduled);
+}
+
+void ControlService::run() {
+  std::function<void()> task;
+  while (queue_.pop(task)) {
+    task();
+  }
+  Motor::applyStateNoDelay();
+}
+
+}  // namespace control
+}  // namespace robocar
---
+++ src/control/control_service.h
@@ -0,0 +1,56 @@
+#pragma once
+
+#include <atomic>
+
+#include "control/motor.h"
+#include "control/vehicle.h"
+#include "runtime/mpsc_queue.h"
+#include "runtime/service_thread.h"
+
+namespace robocar {
+namespace control {
+
+/**
+ * Service responsible for controlling the car.
+ */
+class ControlService {
+ public:
+  ControlService();
+
+  // Adjusts vehicle state with delta values.
+  void adjustVehicleState(double throttleDelta, double steeringAngleDelta);
+  // Sets vehicle state with absolute values.
+  void setThrottle(double throttle);
+  void setSteeringAngle(double steeringAngle);
+  // Sets the motor state
+  void setMotorState(Motor::State state);
+
+  // API to read the current state
+  double throttle() const noexcept {
+    return throttle_.load(std::memory_order_relaxed);
+  }
+  double steeringAngle() const noexcept {
+    return steeringAngle_.load(std::memory_order_relaxed);
+  }
+  Motor::State state() const noexcept {
+    return state_.load(std::memory_order_relaxed);
+  }
+
+ private:
+  static constexpr size_t kTaskQueueSize = 1000;
+  static constexpr size_t kExecutionRateHz = 100;
+
+  Vehicle vehicle_;
+  runtime::MPSCQueue<std::function<void()>> queue_;
+  runtime::ServiceThread thread_;
+
+  // This class keeps a thread-safe copy of the vehicle state.
+  std::atomic<double> throttle_;
+  std::atomic<double> steeringAngle_;
+  std::atomic<Motor::State> state_;
+
+  void run();
+};
+
+}  // namespace control
+}  // namespace robocar
